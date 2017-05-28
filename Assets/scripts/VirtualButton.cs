df2949c48c32163989da0aec517a94d17468d040
magic <magic@WeiHans-iMac.local>
2017-05-28 19:05:57 +0800

    button

---
+++ Assets/scripts/VirtualButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualButton : MonoBehaviour {
+
+	public delegate void VirtualButtonClicked ();
+	public event VirtualButtonClicked OnButtonClicked;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public void NotifyButtonClicked () {
+	
+		if (OnButtonClicked != null) {
+			OnButtonClicked ();
+		}
+	}
+}
